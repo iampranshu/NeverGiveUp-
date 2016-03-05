8019a659614202fe36a99a78b934d996c455e08e
Pranshu Sharma <pranshusharma@simplycloud.in>
2016-03-05 13:19:23 +0530

    reducer

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import  App  from './containers/App'
-import { createStore } from 'redux'
-import rooReducer from './reducers'
-import configureStore from './store/configureStore'
+import { combineReducers } from 'redux'
+import { SUBMIT_ACTION1 } from '../actions'
 
-const store = configureStore(rooReducer)
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+
+const initialState = {
+  isFetching: true
+}
+export default function rooReducer(state=initialState, action){
+  console.log('hello')
+  switch(action.type){
+    case SUBMIT_ACTION1:
+    state= Object.assign({}, state, {input1: action.input})
+    console.log(action)
+    return state
+    default:
+    return state
+  }
+}
